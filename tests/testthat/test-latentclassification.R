e4cadcfc90b00181402477df19e78ec00c4ce5af
Seoyeon Ok <ooksy@tamu.edu>
2023-12-10 15:43:00 -0600

    Change some test files

--- tests/testthat/test-latentclassification.R
+++ tests/testthat/test-latentclassification.R
@@ -11,22 +11,24 @@ w3 <- cbind(rnorm(5, mean = -1, sd = 0.1), rnorm(5, mean = 1, sd = 0.1))
 w4 <- cbind(rnorm(10, mean = -1, sd = 0.1), rnorm(10, mean = -1, sd = 0.1))
 w <- rbind(w1, w2, w3, w4)
 
+
 test_that("tests on error message", {
-  expect_message(latentclassification(z1, w1, 4), "dimension of both z and w should be 2")
+  expect_error(latentclassification(z2, w2, 4), "dimension of both z and w should be 2")
 })
 
 test_that("tests on error message", {
-  expect_message(latentclassification(z2, w2, 4), "dimension of both z and w should be 2")
+  expect_error(latentclassification(z, w, 0), "n_cluster should be at least 2")
 })
 
 test_that("tests on error message", {
-  expect_message(latentclassification(z, w, 0), "n_cluster should be at least 2")
+  expect_error(latentclassification(z, w, 1), "n_cluster should be at least 2")
 })
 
 test_that("tests on error message", {
-  expect_message(latentclassification(z, w, 1), "n_cluster should be at least 2")
+  expect_error(latentclassification(z, w, -1), "n_cluster should be at least 2")
 })
 
 test_that("tests on error message", {
-  expect_message(latentclassification(z, w, -1), "n_cluster should be at least 2")
+  expect_no_error(latentclassification(z, w, 4))
 })
+
